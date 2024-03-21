c8c66231cddb2000a72e17c9f988fa2e8bfc44b4
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-03-21 11:32:42 +0000

    Practice

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+import Check from "./chai"
+
+function App() {
+  return (
+    <Check />
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+// Creating One Function
+function Check(){
+    return(
+        <h2>Chai is Ready</h2>
+    )
+}
+export default Check
