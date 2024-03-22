87d5fb7002351f7a7d68f8822e771faa939e5d7f
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-03-22 10:28:36 +0000

    Something Change

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // import Check from './chai.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App Coffee</h1>
+        </div>
+    )
+}
+
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
-    <App />
+
+    <MyApp /> 
 )
