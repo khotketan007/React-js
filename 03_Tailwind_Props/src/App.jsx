744064b9d6d4f6c42b46dac5f17e1a13fc62eee4
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-03-26 10:42:40 +0000

    Code

--- 03_Tailwind_Props/src/App.jsx
+++ 03_Tailwind_Props/src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-400 text-white p-10 rounded-3xl mb-5'>Tailwind Test</h1>
+    <h1>Tailwind Test</h1>
     </>
   )
 }
