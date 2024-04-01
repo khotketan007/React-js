18b4e46b2a17631a172130b989d78ea8852efd0e
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-04-01 06:58:35 +0000

    React Router

--- 07ReactRouter/src/Layout.jsx
+++ 07ReactRouter/src/Layout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function Layout(){
     return(
         <>
-            <header/>
+            <Header/>
             <Outlet/>
             <Footer/>
         </>
--- 07ReactRouter/src/main.jsx
+++ 07ReactRouter/src/main.jsx
@@ -15,7 +15,8 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home/>
-      }, {
+      }, 
+      {
         path: "",
         element: <About/>
       }
