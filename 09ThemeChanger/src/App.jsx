3ab60563196d464b0a549837f91aa7dacaebd79c
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-04-02 15:21:37 +0000

    Project 09

--- 09ThemeChanger/src/App.jsx
+++ 09ThemeChanger/src/App.jsx
@@ -1,25 +1,23 @@
-
+import { useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/Theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [themeMode, setThememode] = useState("light")
 
   return (
-    <>
-    
-<div className="flex flex-wrap min-h-screen items-center">
-                <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        {/* Theme Button */}
-                    </div>
-
-                    <div className="w-full max-w-sm mx-auto">
-                       {/* Card */}
-                    </div>
-                </div>
-            </div>
-
-    </>
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  {/* Theme Button */}
+              </div>
+              <div className="w-full max-w-sm mx-auto">
+                  {/* Card */}
+              </div>
+          </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
